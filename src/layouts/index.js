99cc64c02951bc5f443208c8d1eef0903f47fdbd
erick <erick@hotmail.ca>
2018-03-31 15:30:53 -0400

    Add fontawesome

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,13 +3,18 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import Carousel from '../utils/carousel'
 import './all.scss'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Lebrun Laboratory" />
+    <Helmet title="Lebrun Laboratory">
+      <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+    </Helmet>
     <Navbar />
     <div>{children()}</div>
+    <Footer />
   </div>
 )
 
