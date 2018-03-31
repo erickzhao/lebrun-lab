b0b940b50686cdfc53a84810517d611969add428
erick <erick@hotmail.ca>
2018-03-31 15:30:49 -0400

    Add carousel

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,10 +7,39 @@ export default class IndexPage extends React.Component {
     const { edges: posts } = data.allMarkdownRemark
 
     return (
+      <div>
+      <section class="hero is-medium has-carousel">
+        <div class="hero-carousel carousel-animated carousel-animate-fade" data-autoplay="true">
+          <div class='carousel-container'>
+            <div class='carousel-item has-background is-active'>
+              <img class="is-background" src="https://wikiki.github.io/images/merry-christmas.jpg" alt="" />
+            </div>
+            <div class='carousel-item has-background'>
+              <img class="is-background" src="https://wikiki.github.io/images/singer.jpg" alt="" />
+            </div>
+            <div class='carousel-item has-background'>
+              <img class="is-background" src="https://wikiki.github.io/images/sushi.jpg" alt="" />
+            </div>
+            <div class='carousel-item has-background'>
+              <img class="is-background" src="https://wikiki.github.io/images/life.jpg" alt="" />
+            </div>
+          </div>
+          <div class="carousel-navigation is-overlay">
+            <div class="carousel-nav-left">
+              <i class="fa fa-chevron-left" aria-hidden="true"></i>
+            </div>
+            <div class="carousel-nav-right">
+              <i class="fa fa-chevron-right" aria-hidden="true"></i>
+            </div>
+          </div>
+        </div>
+        <div class="hero-body has-text-centered">
+        </div>
+      </section>
       <section className="section">
         <div className="container">
           <div className="content">
-            <h1 className="has-text-weight-bold is-size-2">Latest Stories</h1>
+            <h1 className="has-text-weight-bold is-size-2">News</h1>
           </div>
           {posts
             .filter(post => post.node.frontmatter.templateKey === 'news-post')
@@ -39,6 +68,8 @@ export default class IndexPage extends React.Component {
             ))}
         </div>
       </section>
+      </div>
+      
     )
   }
 }
