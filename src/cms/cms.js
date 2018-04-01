2ef412087a050d7d55981f82f05395be2c5504d5
erick <erick@hotmail.ca>
2018-03-31 20:58:59 -0400

    Fix cms oops

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,9 +2,7 @@ import CMS from 'netlify-cms'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import NewsPostPreview from './preview-templates/NewsPostPreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
 
 CMS.registerPreviewStyle('/styles.css')
 CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('news', NewsPostPreview)
