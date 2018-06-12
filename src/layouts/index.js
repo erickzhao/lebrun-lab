6e5735709a45be2e2e8dd5d3bf2d5557f40f36b2
erick <erick@hotmail.ca>
2018-06-12 01:06:25 -0400

    Fix random crap

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
 import Navbar from '../components/Navbar'
-import Carousel from '../utils/carousel'
 import './all.scss'
 
 const TemplateWrapper = ({ children }) => (
