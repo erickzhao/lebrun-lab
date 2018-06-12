4d9eb9174ec67f47a246c8fd2c3c6b4b77de9fab
erick <erick@hotmail.ca>
2018-06-12 01:06:20 -0400

    Map component

---
+++ src/components/Map.js
@@ -0,0 +1,8 @@
+import React from 'react';
+const Map = () => (<iframe
+  frameborder="0" style={{width: '100%', height: '400px', border:'0'}}
+  src="https://www.google.com/maps/embed/v1/place?key=AIzaSyCZ6JNjNv5n4Hzzw04K8ffErcBOWatdFzg 
+    &q=McGill University Health Centre" allowfullscreen>
+</iframe>)
+
+export default Map
