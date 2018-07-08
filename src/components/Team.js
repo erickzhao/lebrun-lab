6912f77fc5623c451bba18f8f549cbbf060da659
erick <erick@hotmail.ca>
2018-07-08 12:53:47 -0400

    Separate types of team members on page

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,28 +1,23 @@
 import React from 'react'
 
 export default ({ members }) => (
-  <div>
-    <section className="section">
-    <div className="container">
-      <div className="columns is-multiline">
-            {members.map(m => (
-              <div className="column is-one-third-desktop is-half-tablet is-full-mobile" key={m.name}>
-                <div className="card ecse--card-equal-height">
-                  <div className="card-image">
-                    <figure className="is-square">
-                      <img className="is-center" src={m.photo} alt={m.name}/>
-                    </figure>
-                  </div>
-                  <div className="card-content">
-                    <p className="title is-4">{m.name}</p>
-                    <p className="subtitle is-6">{m.position}</p>
-                    <p>{m.description}</p>
-                  </div>
+  <div className="container">
+    <div className="columns is-multiline">
+          {members.map(m => (
+            <div className="column is-one-third-desktop is-half-tablet is-full-mobile" key={m.name}>
+              <div className="card card-equal-height">
+                <div className="card-image">
+                  <figure className="is-square">
+                    <img className="is-center" src={m.photo} alt={m.name}/>
+                  </figure>
+                </div>
+                <div className="card-content">
+                  <p className="title is-4">{m.name}</p>
+                  <p>{m.description}</p>
                 </div>
               </div>
-            ))}
-        </div>
-    </div>
-    </section>
+            </div>
+          ))}
+      </div>
   </div>
 )
--- src/templates/team-page.js
+++ src/templates/team-page.js
@@ -3,10 +3,30 @@ import Header from "../components/Header"
 import Team from "../components/Team"
 
 export default ({ data }) => {
+  const positions = {
+    associate: "Research Associates",
+    postdoc: "Post-Doctorate Fellows",
+    grad: "Graduate Students",
+    assistant: "Research Assistants",
+    undergrad: "Undergraduate Students"
+  }
   return (
     <div>
       <Header {...data.markdownRemark.frontmatter}/>
-      <Team {...data.markdownRemark.frontmatter}/>
+      { Object.keys(positions).map(pos => {
+        const members = data.markdownRemark.frontmatter.members.filter(m => m.position === pos);
+        if (members.length > 0) {
+          console.log(members);
+          return (
+            <div className="container" key={pos}>
+              <h2 className="title">{positions[pos]}</h2>
+              <Team members={members}/>
+            </div>
+          )
+        }
+      }
+      )}
+      
     </div>
   )
 }
