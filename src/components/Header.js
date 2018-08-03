9d90d405696dc09d9910a274c07233562e0afbeb
Erick Zhao <erick.zhao@mail.mcgill.ca>
2018-08-02 23:31:13 -0400

    Add gradient to header image

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,9 @@ const Header = ({ title, heading, photo }) => (
     className="hero is-medium is-primary is-bold"
     style={{
       background: photo
-        ? `url(${photo}) no-repeat center center / cover fixed`
-        : ``
+        ? `linear-gradient(to bottom, rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(${photo}) no-repeat center center / cover fixed`
+        : ``,
+      marginBottom: '6rem'
     }}
   >
     <div className="hero-body">
