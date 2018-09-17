70142a01a44f8a1433742aaf44f96e2cbd3121e4
erick <erick.zhao@mail.mcgill.ca>
2018-09-17 01:08:35 -0400

    Add images to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,19 @@
 import React from "react";
-import Link from "gatsby-link";
+import crpLogo from "../img/crp.png";
+import fomLogo from "../img/fom.png";
 
 const Footer = () => (
-  <footer className="footer">
+  <footer className="footer has-text-centered">
+    <div className="container" style={{maxWidth: '700px'}}>
+      <div className="columns">
+        <div className="column">
+          <img src={crpLogo} style={{height: '80px'}}/>
+        </div>
+        <div className="column">
+          <img src={fomLogo}/>
+        </div>
+      </div>
+    </div>
     <p className="content has-text-centered">
       Copyright © 2018 Lebrun Laboratory. All rights reserved.
     </p>
