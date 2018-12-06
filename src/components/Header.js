37787f0add6b5057d60951b3cd477404af1c5890
Erick Zhao <erick.zhao@mail.mcgill.ca>
2018-12-06 02:33:37 -0500

    Adjust header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,14 +8,14 @@ const Header = ({ title, subtitle, image }) => (
       background: image
         ? `linear-gradient(to bottom, rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(${image}) no-repeat center center / cover fixed`
         : ``,
-      marginBottom: '6rem'
+      marginBottom: '3rem'
     }}
   >
     <div className="hero-body">
       <Helmet>
         <title>{title} - J.J. Lebrun Laboratory</title>
       </Helmet>
-      <div className="container">
+      <div className="container has-text-centered">
         <h1 className="title is-1">{title}</h1>
         <h2 className="subtitle">{subtitle}</h2>
       </div>
